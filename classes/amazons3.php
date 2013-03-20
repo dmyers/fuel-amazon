bf480b897162935c88202703f9c8714d37dd0de6
Derek Myers <dmyers@static.com>
2013-03-20 15:26:52 -0500

    Added new getObject function which uses default bucket

--- classes/amazons3.php
+++ classes/amazons3.php
@@ -8,7 +8,7 @@
  * @author		Derek Myers
  * @link		https://github.com/dmyers/fuel-amazons3
  */
- 
+
 namespace AmazonS3;
 
 require_once PKGPATH.'amazons3'.DS.'vendor'.DS.'amazons3'.DS.'S3.php';
@@ -18,8 +18,8 @@ class AmazonS3 extends \S3
 	/**
 	 * loaded amazon s3 instance
 	 */
-	protected static $_instance = null;
-
+	protected static $_instance;
+	
 	/**
 	 * Initialize by loading config
 	 */
@@ -29,7 +29,7 @@ class AmazonS3 extends \S3
 		
 		self::$useExceptions = true;
 	}
-
+	
 	/**
 	 * Returns a new amazon s3 object.
 	 *
@@ -46,14 +46,14 @@ class AmazonS3 extends \S3
 		if (empty($config['access_key_id'])) {
 			throw new \AmazonS3Exception('You must set the access_key_id config');
 		}
-
+		
 		if (empty($config['secret_access_key'])) {
 			throw new \AmazonS3Exception('You must set the secret_access_key config');
 		}
 		
 		return new static($config['access_key_id'], $config['secret_access_key'], $config['use_ssl']);
 	}
-
+	
 	/**
 	 * create or return the amazons3 instance
 	 *
@@ -66,31 +66,40 @@ class AmazonS3 extends \S3
 		if (self::$_instance === null) {
 			self::$_instance = self::forge();
 		}
-
+		
 		return self::$_instance;
 	}
-
+	
 	public static function putObject($input, $bucket = null, $uri, $acl = null, $metaHeaders = array(), $requestHeaders = array(), $storageClass = self::STORAGE_CLASS_STANDARD)
 	{
 		if (empty($bucket)) {
 			$bucket = \Config::get('amazons3.default_bucket');
 		}
-
+		
 		if (empty($acl)) {
 			$acl = \Config::get('amazons3.default_acl', self::ACL_PRIVATE);
 		}
-
+		
 		return parent::putObject($input, $bucket, $uri, $acl, $metaHeaders, $requestHeaders, $storageClass);
 	}
-
+	
 	public static function putObjectFile($file, $bucket = null, $uri, $acl = null, $metaHeaders = array(), $contentType = null)
 	{
 		return self::putObject(self::inputFile($file), $bucket, $uri, $acl, $metaHeaders, $contentType);
 	}
-
+	
 	public static function putObjectString($string, $bucket = null, $uri, $acl = null, $metaHeaders = array(), $contentType = 'text/plain')
 	{
 		return self::putObject($string, $bucket, $uri, $acl, $metaHeaders, $contentType);
+	}
+	
+	public static function getObject($bucket = null, $uri, $saveTo = false)
+	{
+		if (empty($bucket)) {
+			$bucket = \Config::get('amazons3.default_bucket');
+		}
+		
+		return parent::getObject($bucket, $uri, $saveTo);
 	}
 }
 
